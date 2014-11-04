db59fbd699261feaeb045653d03891f722db78d3
William G Hatch <willghatch@gmail.com>
2014-11-03 23:31:37 -0700

    make prompt customizeable

--- prompt/prompt.zsh
+++ prompt/prompt.zsh
@@ -6,53 +6,50 @@ fi
 
 setopt prompt_subst
 
-PS1_time='%F{cyan}%T'
-PS1_hostcolor="%F{yellow}"
-PS1_user_host='%F{green}[%B%n%b${PS1_hostcolor}@%B%m%b%F{green}]'
-PS1_dir='%F{blue}%B%~'
+typeset -Ag VZSH_PROMPT_STYLES
+VZSH_PROMPT_STYLES[time]="%b%F{cyan}"
+VZSH_PROMPT_STYLES[host]="%B%F{yellow}"
+VZSH_PROMPT_STYLES[userhost_brackets]="%b%F{green}"
+VZSH_PROMPT_STYLES[username]="%B%F{green}"
+VZSH_PROMPT_STYLES[at]="%b%F{green}"
+VZSH_PROMPT_STYLES[dir]="%B%F{blue}"
+VZSH_PROMPT_STYLES[histnum]="%b%F{yellow}"
+VZSH_PROMPT_STYLES[dollar]="%b%F{default}"
+VZSH_PROMPT_STYLES[git_master]="%b%F{white}"
+VZSH_PROMPT_STYLES[git_other]="%b%F{red}"
+typeset -Ag VZSH_KEYMAP_INDICATOR
+VZSH_KEYMAP_INDICATOR[main]="%K{magenta}%F{black}I%k"
+VZSH_KEYMAP_INDICATOR[viins]="%K{magenta}%F{black}I%k"
+VZSH_KEYMAP_INDICATOR[emacs]="%K{magenta}%F{black}I%k"
+VZSH_KEYMAP_INDICATOR[vicmd]="%K{blue}%F{black}N%k"
+VZSH_KEYMAP_INDICATOR[opp]="%K{yellow}%F{black}O%k"
+VZSH_KEYMAP_INDICATOR[vivis]="%K{green}%F{black}V%k"
+VZSH_KEYMAP_INDICATOR[vivli]="%K{green}%F{black}V%k"
+VZSH_KEYMAP_INDICATOR[keymap_unlisted]="%K{white}%F{black}?%k"
 PS1_cmd_stat='%b%F{cyan}<%(?,%F{green}%?,%F{red}%?)%F{cyan}>'
-PS1_hist='%F{yellow}%h'
-PS1_end='%F{default}%# '
-PS1_endl='
-'
-PS1_vi_state="\$VIMODE"
+
 getGitBranchForPrompt() {
     branch=$(git branch 2>/dev/null | fgrep '*')
     if [ "$branch" = "* master" ]
     then
-        branch="%F{grey}[%F{white}${branch:2}%F{grey}]"
+        branch="%F{grey}[${VZSH_PROMPT_STYLES[git_master]}${branch:2}%F{grey}] "
     elif [ -n "$branch" ]
     then
-        branch="%F{grey}[%F{red}${branch:2}%F{grey}]"
+        branch="%F{grey}[${VZSH_PROMPT_STYLES[git_other]}${branch:2}%F{grey}] "
     fi
     echo $branch
 }
-PS1_git_branch="\$(getGitBranchForPrompt)"
 
 update-prompt() {
-    local VIMODE=$ZSH_CUR_KEYMAP
-    if [[ "$VIMODE" = "viins" || "$VIMODE" = "emacs" || "$VIMODE" = "main" ]]
-    then
-        VIMODE="%K{magenta}%F{black}I%k"
-    elif [[ "$VIMODE" = "vicmd" ]]
-    then
-        VIMODE="%K{blue}%F{black}O%k"
-    elif [[ "$VIMODE" = "opp" ]]
-    then
-        VIMODE="%K{yellow}%F{black}O%k"
-    elif [[ "$VIMODE" = "vivis" || "$VIMODE" = "vivli" ]]
-    then
-        VIMODE="%K{green}%F{black}V%k"
-    else
-        VIMODE="%K{white}%F{black}$VIMODE%k"
+    local -A s
+    set -A s ${(kv)VZSH_PROMPT_STYLES}
+    local k=$VZSH_KEYMAP_INDICATOR[$ZSH_CUR_KEYMAP]
+    if [[ -z "$k" ]]; then
+        k=$VZSH_KEYMAP_INDICATOR[keymap_unlisted]
     fi
 
-    # if the user has set up something custom, use it
-    if [[ -n "$VZSH_PS1_UPDATE_FUNC" ]]; then
-        $VZSH_PS1_UPDATE_FUNC
-    else
-        PS1="${PS1_time} ${PS1_user_host} ${PS1_git_branch} ${PS1_dir} ${PS1_cmd_stat}${PS1_endl}${PS1_vi_state}${PS1_hist}${PS1_end}"
-    fi
+    PS1="$s[time]%T ${s[userhost_brackets]}[${s[username]}%n${s[at]}@${s[host]}%m${s[userhost_brackets]}] \$(getGitBranchForPrompt)${s[dir]}%~ ${PS1_cmd_stat}
+${k} ${s[histnum]}%h ${s[dollar]}%# "
 
     zle reset-prompt
 }
